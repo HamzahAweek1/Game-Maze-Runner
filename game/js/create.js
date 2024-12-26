e8e92a79bc17415574287db2d60881f41fc809c9
HamzahAweek1 <aweekhamzah@gmail.com>
2024-12-26 10:12:21 +0200

    Generated Maze layout

--- game/js/create.js
+++ game/js/create.js
@@ -29,4 +29,32 @@ function create(index) {
   const tileHeight = 40;
   const collectorsWidth = 30;
   const collectorsHeight = 30;
+
+  for (let row = 0; row < mazeNumber.length; row++) {
+    for (let col = 0; col < mazeNumber[row].length; col++) {
+      if (mazeNumber[row][col] === 1) {
+        const wall = platforms
+          .create(col * tileWidth, row * tileHeight, "tile")
+          .setOrigin(0, 0)
+          .refreshBody();
+        wall.setDisplaySize(tileWidth, tileHeight);
+      } else if (mazeNumber[row][col] === 2) {
+        const coin = coins
+          .create(col * tileWidth, row * tileHeight, "coin")
+          .setOrigin(0, 0)
+          .refreshBody();
+        coin.setCollideWorldBounds(true);
+        coin.setDisplaySize(collectorsWidth, collectorsHeight);
+      } else if (mazeNumber[row][col] === 3) {
+        const prize = trophy
+          .create(col * tileWidth, row * tileHeight, "prize")
+          .setOrigin(0, 0)
+          .refreshBody();
+        prize.setCollideWorldBounds(true);
+        prize.setDisplaySize(collectorsWidth, collectorsHeight);
+      }
+    }
+  }
+  
 }
+
